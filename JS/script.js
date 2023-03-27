e88109b2d59c802c4929ee17a81f6e66d83674ad
WoWdeadlock <chandithamahanama@gmail.com>
2023-03-28 01:32:42 +0530

    WIP

--- JS/script.js
+++ JS/script.js
@@ -1,32 +1,104 @@
 //Grid Creation
 
 let gridContainer = document.querySelector("#grid-container");
+let radialMenu =document.querySelector(".img-radial");
+
+let angle = 0;
 
 let screenSize = 512;
-let rows = 64;                       
 
-for (let i = 0; i < rows; i++) {
+gridCreator(8);
 
-    let row = document.createElement("div");
-    row.className = "grid-row";
+function gridCreator(rows) {
+
+    gridContainer.replaceChildren();
 
     for (let i = 0; i < rows; i++) {
 
-        let squareDimension = screenSize / rows;
-        let squareDimensionsInPixelsWidth = squareDimension + "px";
-        let squareDimensionsInPixelsHeight = squareDimension + "px";
+        let row = document.createElement("div");
+        row.className = "grid-row";
+    
+        for (let i = 0; i < rows; i++) {
+    
+            let squareDimension = screenSize / rows;
+            let squareDimensionsInPixelsWidth = squareDimension + "px";
+            let squareDimensionsInPixelsHeight = squareDimension + "px";
+    
+            let squareDiv = document.createElement("div");
+            squareDiv.className = "square";
+            squareDiv.style.height = squareDimensionsInPixelsHeight;
+            squareDiv.style.width = squareDimensionsInPixelsWidth;
+            row.appendChild(squareDiv);
+    
+        }
+    
+        gridContainer.appendChild(row);
+    
+    }    
 
-        let squareDiv = document.createElement("div");
-        squareDiv.className = "square";
-        squareDiv.style.height = squareDimensionsInPixelsHeight;
-        squareDiv.style.width = squareDimensionsInPixelsWidth;
-        row.appendChild(squareDiv);
+}
+
+console.log("before radial event mlistener");
+
+radialMenu.addEventListener("click", () => {
+
+    angle += 90;
+    radialMenu.style.transform = "rotate(" + angle + "deg)";
+
+    if (angle == 360) {
+
+        angle = 0;
 
     }
 
-    gridContainer.appendChild(row);
+    switch (angle) {
+        case 0:
+            gridCreator(8);
+            startGame();
+            break;
+        
+        case 90:
+            gridCreator(16);
+            startGame();
+            break;
 
-}
+        case 180:
+            gridCreator(32);
+            break;
+
+        case 270:
+            gridCreator(64);
+            break;
+
+        default:
+            gridCreator(8);
+            break;
+    }
+
+})
+
+// for (let i = 0; i < rows; i++) {
+
+//     let row = document.createElement("div");
+//     row.className = "grid-row";
+
+//     for (let i = 0; i < rows; i++) {
+
+//         let squareDimension = screenSize / rows;
+//         let squareDimensionsInPixelsWidth = squareDimension + "px";
+//         let squareDimensionsInPixelsHeight = squareDimension + "px";
+
+//         let squareDiv = document.createElement("div");
+//         squareDiv.className = "square";
+//         squareDiv.style.height = squareDimensionsInPixelsHeight;
+//         squareDiv.style.width = squareDimensionsInPixelsWidth;
+//         row.appendChild(squareDiv);
+
+//     }
+
+//     gridContainer.appendChild(row);
+
+// }
 
 // UI
 
@@ -107,3 +179,52 @@ rowSelector.forEach(row => {
     
 });
 
+function startGame() {
+
+    let rowSelector = gridContainer.querySelectorAll(".grid-row");
+    let mouseDownVariable = false;  
+    
+    let selection = "pencil";
+    
+    rowSelector.forEach(row => {
+        
+        let squareSelector = row.querySelectorAll(".square");
+    
+        squareSelector.forEach(square => {
+    
+            square.addEventListener("mousedown", () => {
+    
+                mouseDownVariable = true;
+    
+            })
+    
+            square.addEventListener("mouseup", () => {
+    
+                mouseDownVariable = false;
+    
+            })
+    
+            square.addEventListener("mousemove", () => {
+    
+                if (mouseDownVariable) {
+    
+                    switch (selection) {
+                        case "eraser":
+                            square.style.backgroundColor = "#FBFFF1";
+                            break;
+                    
+                        case "pencil":
+                            square.style.backgroundColor = "black";
+                            break;
+                    }
+    
+                }
+    
+            })
+            
+    
+        });
+        
+    });
+
+}
