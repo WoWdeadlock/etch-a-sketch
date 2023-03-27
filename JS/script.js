72b2881272c193516073306102472fd5f7874bee
WoWdeadlock <chandithamahanama@gmail.com>
2023-03-27 15:39:02 +0530

    Implemented sketching window

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,23 @@
 let gridContainer = document.querySelector("#grid-container");
 
-for (let i = 0; i < 4; i++) {
+let screenSize = 512;
+let rows = 16;                       
+
+for (let i = 0; i < rows; i++) {
 
     let row = document.createElement("div");
     row.className = "grid-row";
 
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < rows; i++) {
+
+        let squareDimension = screenSize / rows;
+        let squareDimensionsInPixelsWidth = squareDimension + "px";
+        let squareDimensionsInPixelsHeight = squareDimension + "px";
 
         let squareDiv = document.createElement("div");
         squareDiv.className = "square";
+        squareDiv.style.height = squareDimensionsInPixelsHeight;
+        squareDiv.style.width = squareDimensionsInPixelsWidth;
         row.appendChild(squareDiv);
 
     }
@@ -16,3 +25,40 @@ for (let i = 0; i < 4; i++) {
     gridContainer.appendChild(row);
 
 }
+
+let rowSelector = gridContainer.querySelectorAll(".grid-row");
+
+let mouseDownVariable = 0;
+
+rowSelector.forEach(row => {
+    
+    let squareSelector = row.querySelectorAll(".square");
+
+    squareSelector.forEach(square => {
+
+        square.addEventListener("mousedown", () => {
+
+            mouseDownVariable = 1;
+
+        })
+
+        square.addEventListener("mouseup", () => {
+
+            mouseDownVariable = 0;
+
+        })
+
+        square.addEventListener("mousemove", () => {
+
+            if (mouseDownVariable == 1) {
+
+                square.classList.add("hovered");
+
+            }
+
+        })
+        
+
+    });
+    
+});
